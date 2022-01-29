328ed4942436b9b70181feb1e6a5d3ca7c7719a7
fatouBintouRassoul <77291385+fatouBintouRassoul@users.noreply.github.com>
2022-01-29 14:53:07 +0100

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Admin extends User {
+    private String login;
+    private String password;
+    private String role;
+
+    public Admin() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+}
