96b53d0032364d51e1fd50ab6961768b550c480d
fatouBintouRassoul <77291385+fatouBintouRassoul@users.noreply.github.com>
2022-01-29 14:56:28 +0100

    Create Professeur.java

---
+++ Professeur.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Professeur extends User {
+    private String nci;
+    private String grade;
+
+    public Professeur() {
+    }
+
+    public String getNci() {
+        return nci;
+    }
+
+    public void setNci(String nci) {
+        this.nci = nci;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+    
+    
+}
