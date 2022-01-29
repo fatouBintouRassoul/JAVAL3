d1f011b0caf25b7bd9c7106ce2a9244fb6937231
fatouBintouRassoul <77291385+fatouBintouRassoul@users.noreply.github.com>
2022-01-29 14:54:42 +0100

    Create Classe.java

---
+++ Classe.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Classe {
+    private int id;
+    private String filiere;
+    private String niveau;
+
+    public Classe() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFiliere() {
+        return filiere;
+    }
+
+    public void setFiliere(String filiere) {
+        this.filiere = filiere;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
+    }
+    
+    
+     
+}
