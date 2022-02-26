88fcd1b5e70839d377a4ca6b4221ee9af02a4af4
fatouBintouRassoul <77291385+fatouBintouRassoul@users.noreply.github.com>
2022-02-26 10:58:55 +0100

    Create DaoEtudiant.java

---
+++ DaoEtudiant.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import entity.Etudiant;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DaoEtudiant {
+    public int insert(Etudiant etudiant){
+        return 0;
+}
+    public ArrayList<Etudiant> selectAll(){
+        return null;
+    }
+    
+}
+    
