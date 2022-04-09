02d822a1609d00bece82efda5702bc42962f04e9
fatouBintouRassoul <77291385+fatouBintouRassoul@users.noreply.github.com>
2022-04-09 09:02:45 +0200

    Create DaoInscription1.java

---
+++ DaoInscription1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DaoInscription {
+    public int insert(int idEtudiant, int idClasse){
+        return 0;
+    }
+    
+}
