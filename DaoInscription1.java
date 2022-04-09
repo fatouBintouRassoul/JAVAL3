695070aea2cd6ab4c570945388a80a932f238d06
fatouBintouRassoul <77291385+fatouBintouRassoul@users.noreply.github.com>
2022-04-09 09:15:59 +0200

    Update DaoInscription1.java

--- DaoInscription1.java
+++ DaoInscription1.java
@@ -5,6 +5,12 @@
  */
 package dao;
 
+import entity.Etudiant;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Lenovo
@@ -13,5 +19,38 @@ public class DaoInscription {
     public int insert(int idEtudiant, int idClasse){
         return 0;
     }
-    
+    public int insert(Etudiant objet) {
+        int id=0;
+        DaoMysql mysql = new DaoMysql();
+        mysql.ouvrirConnexionBD();
+        mysql.preparerRequete(SQL_INSERT);
+        try {
+            mysql.getPs().setString(1,objet.getMatricule());
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoEtudiant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            mysql.getPs().setString(2,objet.getTuteur());
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoEtudiant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        mysql.executeMisAJour();
+        ResultSet rs = null;
+        try {
+            rs = mysql.getPs().getGeneratedKeys();
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoEtudiant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            if (rs.next()){
+                id = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoEtudiant.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //System.out.println(id);
+        mysql.closeConnexion();
+       return id;
+    }
+
 }
